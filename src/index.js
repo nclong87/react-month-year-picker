4976de5fe59209b912a67bf616952511089a51ee
Shubham Shukla <shk.shubham@gmail.com>
2019-04-25 15:56:04 -0700

    Fixed less import error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import './month-picker.less';
+import './monthYearPicker.less';
 
 function RenderMonth(props){
   const {
